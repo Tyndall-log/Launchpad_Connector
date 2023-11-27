91d21993c64d61bf001774236d1926fcdb6e7ffb
IreneStella <eunsu0402@naver.com>
2023-11-27 16:06:15 +0900

    런치패드 LED 테스트
    런치패드 LED 전송 테스트

--- module/launchpad/console_test/main.h
+++ module/launchpad/console_test/main.h
@@ -1,11 +1,8 @@
 // SPDX-FileCopyrightText: © 2023 Kim Eun-su <eunsu0402@gmail.com>
 // SPDX-License-Identifier: LGPL-3.0-linking-exception
 
-#include "main.h"
+#include "launchpad.h"
 
-int main()
-{
-	test1();
-	//test2();
-	return 0;
-}
+int test1();
+int test2();
+int test3();
